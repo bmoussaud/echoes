8abd7fe6c76b5fed17b6c6bf39f96e77b710ee4d
Benoit Moussaud <bmoussaud@xebialabs.com>
2016-09-08 17:32:51 +0200

    better rendering

--- app/app.py
+++ app/app.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask import render_template
 import os
+import socket
 
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/env/')
 def dump_environment():
-    return render_template('environment.html', environ=os.environ)
+    hostname = os.getenv('HOSTNAME', None)
+    if hostname is None:
+        hostname = socket.gethostname()
+
+    hosts = [line[:-1] for line in open('/etc/hosts', 'r').readlines()]
+    return render_template('environment.html', hostname=hostname, environ=os.environ, hosts=hosts)
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
-
-
+    app.run(debug=True, host='0.0.0.0')
