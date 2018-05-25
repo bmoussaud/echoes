e64a15934579d5f38a56748dd5f52c03125e2d5c
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-05-25 09:27:58 +0200

    XLDeploy package

--- xebialabs/package.sh
+++ xebialabs/package.sh
@@ -11,22 +11,17 @@ pwd
 find /app
 find /xebialabs
 
-#mkdir -p /tmp/tools
-#curl https://github.com/jchraibi/tools/blob/master/zip?raw=true -L -o /tmp/tools/zip
-#chmod a+x /tmp/tools/zip
-echo "-zip--"
-zip
-echo "---"
+APP_VER='1.0'
+XLDEPLOY_URL="http://192.168.64.1:4516"
 
-echo "-curl--"
-curl
-echo "---"
+echo "APP_VER      : $APP_VER"
+echo "XLDEPLOY_URL : $XLDEPLOY_URL"
 
-#sed "s/@APP_VERSION@/$APP_VER/g" /tmp/src/xebialabs/work/deploymentconfig-cart.yaml.bak > /tmp/src/xebialabs/work/deploymentconfig-cart.yaml
-#sed "s/@APP_VERSION@/$APP_VER/g" /tmp/src/xebialabs/deployit-manifest.xml.bak > /tmp/src/xebialabs/deployit-manifest.xml
+sed "s/@APP_VERSION@/$APP_VER/g" /xebialabs/deploymentconfig.yaml > /xebialabs/deploymentconfig.yaml
+sed "s/@APP_VERSION@/$APP_VER/g" /xebialabs/deployit-manifest.xml > /xebialabs/deployit-manifest.xml
 
-#echo "Creating DAR file for XLDEPLOY"
-#cd /tmp/src/xebialabs; /tmp/src/zip -r ../xebialabs.dar ./*; cd ..;
-#unzip -l /tmp/src/xebialabs.dar
-#echo "Push package to XLDEPLOY"
-#curl -uadmin:admin -X POST -H "content-type:multipart/form-data" $XLDEPLOY_URL/deployit/package/upload/xebialabs.dar -F fileData=@/tmp/src/xebialabs.dar
+echo "Creating DAR file for XLDEPLOY"
+cd /xebialabs; zip -r /tmp/xebialabs.dar ./*; cd ..;
+unzip -l /tmp/xebialabs.dar
+echo "Push package to XLDEPLOY"
+curl -uadmin:admin -X POST -H "content-type:multipart/form-data" $XLDEPLOY_URL/deployit/package/upload/xebialabs.dar -F fileData=@/tmp/src/xebialabs.dar
