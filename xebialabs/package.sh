01d8f916404f441a5e8ece35f3b9fa679b69d83f
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-05-25 09:58:24 +0200

    sed in place

--- xebialabs/package.sh
+++ xebialabs/package.sh
@@ -1,15 +1,6 @@
-#oc set build-hook bc/echoes-bc --post-commit --script="sh xebialabs/package.sh"
+#oc set build-hook bc/echoes-bc --post-commit --script="sh /xebialabs/package.sh"
 #https://docs.openshift.org/latest/dev_guide/builds/build_hooks.html
-
-echo "Before assembling"
-echo "uname..."
-uname -a
-echo "find..."
-find .
-echo "-------"
-pwd
-find /app
-find /xebialabs
+find / -ls
 
 APP_VER='1.0'
 XLDEPLOY_URL="http://192.168.99.1:4545"
