c8446cad3b1dd8a439f4500d27008692805f9135
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-05-25 09:34:21 +0200

    fix filedata in curl

--- xebialabs/package.sh
+++ xebialabs/package.sh
@@ -24,4 +24,4 @@ echo "Creating DAR file for XLDEPLOY"
 cd /xebialabs; zip -r /tmp/xebialabs.dar ./*; cd ..;
 unzip -l /tmp/xebialabs.dar
 echo "Push package to XLDEPLOY"
-curl -uadmin:admin -X POST -H "content-type:multipart/form-data" $XLDEPLOY_URL/deployit/package/upload/xebialabs.dar -F fileData=@/tmp/src/xebialabs.dar
+curl -uadmin:admin -X POST -H "content-type:multipart/form-data" $XLDEPLOY_URL/deployit/package/upload/xebialabs.dar -F fileData=@/tmp/xebialabs.dar
